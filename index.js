66949e33e1d8d144579e39fd86a4bb9bea136085
bf0880-boop <bf0880@est.ort.edu.ar>
2025-09-17 11:37:56 -0300

    Termine el trabajo

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import fs, { writeFileSync } from "fs" 
-
+let ordentexto =fs.readFileSync("Cuento/orden.txt", "utf-8")
+let partes = ordentexto.split("\r\n")
 let orden = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136];
-
 let texto = "";
 
-for (let i = 0; i < orden.length; i++){
-    texto = texto + fs.readFileSync("Cuento/parte" + orden[i] + ".txt", "utf8") + "\n";
+for (let i = 0; i < partes.length; i++){
+    texto = texto + fs.readFileSync("Cuento/parte" + partes[i] + ".txt", "utf8") + "\n";
 }
-console.log(texto + "\n");
-fs.writeFileSync("La autopista del sur.txt", texto, "utf-8");
+fs.writeFileSync("Cuento/La autopista del sur.txt", texto, "utf-8");
